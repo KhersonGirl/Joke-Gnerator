358b1ee938598e0f0384496330c95ff25adfb6bc
KhersonGirl <135836740+KhersonGirl@users.noreply.github.com>
2026-05-24 23:33:02 +0200

    AI api integration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 function jokeGenerator(response) {
+
+
   let answer = response.data.answer;
 
   new Typewriter("#joke", {
@@ -9,10 +11,12 @@ function jokeGenerator(response) {
   });
 }
 
-function getAnswer() {
+function getAnswer(event) {
+  event.preventDefault();
+
   let prompt = "Tell me a joke ";
   let context =
-    "You are comedy writer. Always write jokes diffefrently. Do not repitwording or templates. You must write a new joke each time.";
+    "You are comedy writer. Always write jokes diffefrently. Do not repitwording or templates. You must write a new joke each time. Write each line of the joke on a new line using the <br /> tag";
   let apiKey = "e2564f39e1f4c4aa4bf09cftodd0583e";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
