48e887e054f5a845eb52a48e688b135c14adee31
KhersonGirl <135836740+KhersonGirl@users.noreply.github.com>
2026-05-24 23:59:24 +0200

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -14,11 +14,17 @@ function jokeGenerator(response) {
 function getAnswer(event) {
   event.preventDefault();
 
-  let prompt = "Tell me a joke ";
+  let userInput = document.querySelector("#userInput");
+
+  let prompt = `User instructions: Generate a funny joke about ${userInput.value}`;
   let context =
     "You are comedy writer. Always write jokes diffefrently. Do not repitwording or templates. You must write a new joke each time. Write each line of the joke on a new line using the <br /> tag";
   let apiKey = "e2564f39e1f4c4aa4bf09cftodd0583e";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let jokeElement = document.querySelector("#joke");
+  jokeElement.classList.remove("hidden");
+  jokeElement.innerHTML = `<div class="generating">⏳ Generating a joke about ${userInput.value}</div>`;
 
   axios.get(apiUrl).then(jokeGenerator);
 }
